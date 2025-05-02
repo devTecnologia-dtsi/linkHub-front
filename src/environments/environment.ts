a57e4a97344e1a1fa0e9fc42e7d8cf37d33a066f
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-05-02 16:54:45 -0500

    Se realizan ajustes para el lanzamiento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,17 @@ export const environment = {
   baseUrl: 'http://localhost/Api_Catalogos/html/public/index.php',
   // baseUrl: 'http://localhost:80',
   auth: {
-    clientId: "d72569ba-95a4-4dd7-b0d2-f9d6d4d6a223", // Application (client) ID from the app registration
-    authority: "https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300", // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    clientId: 'd72569ba-95a4-4dd7-b0d2-f9d6d4d6a223', // Application (client) ID from the app registration
+    authority:
+      'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+    redirectUri: 'http://10.0.26.31:81/login',
     // redirectUri: 'http://localhost:4200/dashboard',
-    redirectUri: 'http://localhost:81/dashboard',
-    // postLogoutRedirectUri: 'http://localhost:4200/login'
-    postLogoutRedirectUri: 'http://localhost:81/login'
-},
-apiConfig: {
+    // redirectUri: 'http://localhost:81/dashboard',
+    postLogoutRedirectUri: 'http://localhost:4200/login',
+    // postLogoutRedirectUri: 'http://localhost:81/login'
+  },
+  apiConfig: {
     scopes: ['user.read'],
-    uri: 'https://graph.microsoft.com/v1.0/me'
-  }
-}
+    uri: 'https://graph.microsoft.com/v1.0/me',
+  },
+};
