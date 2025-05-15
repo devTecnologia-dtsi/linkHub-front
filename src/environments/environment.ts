f7bcc8804ac692f4b96be31696c278ba66601a5e
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-05-15 09:50:57 -0500

    Se cambia el puerto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
     clientId: 'd72569ba-95a4-4dd7-b0d2-f9d6d4d6a223', // Application (client) ID from the app registration
     authority:
       'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-    redirectUri: 'http://10.0.26.31:8082/login',
+    redirectUri: 'http://10.0.26.31:8083/login',
     // redirectUri: 'http://localhost:4200/dashboard',
     // redirectUri: 'http://localhost:81/dashboard',
-    postLogoutRedirectUri: 'http://10.0.26.31:8082/login',
+    postLogoutRedirectUri: 'http://10.0.26.31:8083/login',
     // postLogoutRedirectUri: 'http://localhost:81/login'
   },
   apiConfig: {
