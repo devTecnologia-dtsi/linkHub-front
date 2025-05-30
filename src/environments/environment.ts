6b9841419be97a4c3316b3218b619c0fef1f43b4
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-05-30 17:09:44 -0500

    se ajustan las variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,17 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost/Api_Catalogos/html/public/index.php',
-  // baseUrl: 'http://localhost:80',
+  // baseUrl: 'http://localhost/Api_Catalogos/html/public/index.php',
+  // baseUrl: 'http://localhost:8084',
+  baseUrl: 'http://10.0.26.31:8084',
   auth: {
-    clientId: 'd72569ba-95a4-4dd7-b0d2-f9d6d4d6a223', // Application (client) ID from the app registration
+    clientId: '0a26838f-0dec-46b7-9e88-38d9cea317d0',
     authority:
       'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-    redirectUri: 'https://linkhub.uniminuto.edu/login',
-    // redirectUri: 'http://localhost:4200/dashboard',
-    // redirectUri: 'http://localhost:81/dashboard',
-    postLogoutRedirectUri: 'https://linkhub.uniminuto.edu/login',
-    // postLogoutRedirectUri: 'http://localhost:81/login'
+    // redirectUri: 'https://linkhub.uniminuto.edu/login',
+    // redirectUri: 'http://localhost:4200/login',
+    redirectUri: 'http://10.0.26.31:8083/login',
+    // postLogoutRedirectUri: 'https://linkhub.uniminuto.edu/login',
+    postLogoutRedirectUri: 'http://10.0.26.31:8083/login',
   },
   apiConfig: {
     scopes: ['user.read'],
