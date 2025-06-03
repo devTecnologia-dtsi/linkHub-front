2b5eadad6d75ace89e228faf969a626a266abbbd
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-06-03 15:55:56 -0500

    Se configuran las variables de entorno del ambiente de produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
     clientId: '0a26838f-0dec-46b7-9e88-38d9cea317d0',
     authority:
       'https://login.microsoftonline.com/b1ba85eb-a253-4467-9ee8-d4f8ed4df300', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
-    // redirectUri: 'https://linkhub.uniminuto.edu/login',
+    redirectUri: 'https://linkhub.uniminuto.edu/login',
     // redirectUri: 'http://localhost:4200/login',
-    redirectUri: 'http://10.0.26.31:8083/login',
-    // postLogoutRedirectUri: 'https://linkhub.uniminuto.edu/login',
-    postLogoutRedirectUri: 'http://10.0.26.31:8083/login',
+    // redirectUri: 'http://10.0.26.31:8083/login',
+    postLogoutRedirectUri: 'https://linkhub.uniminuto.edu/login',
+    // postLogoutRedirectUri: 'http://10.0.26.31:8083/login',
   },
   apiConfig: {
     scopes: ['user.read'],
