2fdff59877f7e8d6ad524425f2d4c61ff4695f4f
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-09-04 11:18:24 -0500

    Se realizan los ajustes recomendados por la herramienta de WAVE

--- src/app/pages/panel/document/document.component.ts
+++ src/app/pages/panel/document/document.component.ts
@@ -1,9 +1,22 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer';
 // interfaces
-import { DrawerFooter, drawerTemplate } from '../../../interface/global.interface';
-import { ColumnItemDocument, DatatableDocument, FormDocument } from '../../../interface/document.interface';
+import {
+  DrawerFooter,
+  drawerTemplate,
+} from '../../../interface/global.interface';
+import {
+  ColumnItemDocument,
+  DatatableDocument,
+  FormDocument,
+} from '../../../interface/document.interface';
 // component and-desing
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -16,9 +29,13 @@ import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzMessageModule, NzMessageService } from 'ng-zorro-antd/message';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 // services
 import { DocumentService } from '../../../service/document/document.service';
 import { GlobalService } from '../../../service/global/global.service';
+// Directives
+import { LimitCharsDirective } from '../../../directive/limit-chars/limit-chars.directive';
+
 @Component({
   selector: 'app-document',
   standalone: true,
@@ -36,48 +53,56 @@ import { GlobalService } from '../../../service/global/global.service';
     NzPopconfirmModule,
     NzToolTipModule,
     NzSwitchModule,
-    ReactiveFormsModule
+    NzCheckboxModule,
+    ReactiveFormsModule,
+    LimitCharsDirective,
   ],
   templateUrl: './document.component.html',
-  styleUrl: './document.component.css'
+  styleUrl: './document.component.css',
 })
 export class DocumentComponent {
-  @ViewChild('drawerDocument', { static: false }) drawerTemplate?: TemplateRef<drawerTemplate>
-  @ViewChild('footerDocument', { static: false }) drawerFooter?: TemplateRef<DrawerFooter>
-  isLoading: boolean = true
-  isSaving: boolean = false
+  @ViewChild('drawerDocument', { static: false })
+  drawerTemplate?: TemplateRef<drawerTemplate>;
+  @ViewChild('footerDocument', { static: false })
+  drawerFooter?: TemplateRef<DrawerFooter>;
+  isLoading: boolean = true;
+  isSaving: boolean = false;
   searchValue: string = '';
 
-  drawerRef: any
+  drawerRef: any;
   dataForm: DatatableDocument = {
     id: '',
     nombre_documento: '',
     activo: false,
-    loading: false
-  }
-  edit: boolean = false // variable usada para saber cuando se esta actualizando el registro
+    loading: false,
+  };
+  edit: boolean = false; // variable usada para saber cuando se esta actualizando el registro
 
-  validateForm: FormGroup<FormDocument> = this.fb.group({ descripcion: ['', [Validators.required]] });
+  validateForm: FormGroup<FormDocument> = this.fb.group({
+    descripcion: ['', [Validators.required]],
+  });
 
   listOfColumns: ColumnItemDocument[] = [
     {
       name: 'id',
       sortOrder: null,
-      sortFn: (a: DatatableDocument, b:DatatableDocument)=> JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
-      showSort: true
+      sortFn: (a: DatatableDocument, b: DatatableDocument) =>
+        JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
+      showSort: true,
     },
     {
       name: 'Documento',
       sortOrder: null,
-      sortFn: (a: DatatableDocument, b: DatatableDocument)=> a.nombre_documento.localeCompare(b.nombre_documento),
-      showSort: true
+      sortFn: (a: DatatableDocument, b: DatatableDocument) =>
+        a.nombre_documento.localeCompare(b.nombre_documento),
+      showSort: true,
     },
     {
       name: 'Acciones',
       sortFn: null,
       sortOrder: null,
-      showSort: false
-    }
+      showSort: false,
+    },
   ];
 
   listOfData: DatatableDocument[] = [];
@@ -89,25 +114,27 @@ export class DocumentComponent {
     private serviceDocument: DocumentService,
     private message: NzMessageService,
     private globalService: GlobalService
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
   }
 
   getData(): void {
-    this.isLoading = true
-    this.serviceDocument.getAllDocuments()
-      .subscribe((data: any) => {
-        this.isLoading = false
-        if (data.ok != undefined && data.ok == false) {
-          this.message.error('¡Ups! Hubo un error al obtener los ambientes', { nzDuration: 2500 })
-          return
-        }
-        this.listOfData = data.data
-        this.listOfDataCopy = data.data
-      })
+    this.isLoading = true;
+    this.serviceDocument.getAllDocuments().subscribe((data: any) => {
+      this.isLoading = false;
+      if (data.ok != undefined && data.ok == false) {
+        this.message.error('¡Ups! Hubo un error al obtener los ambientes', {
+          nzDuration: 2500,
+        });
+        return;
+      }
+      this.listOfData = data.data.map(({ activo, ...rest }: any) => {
+        return { ...rest, activo: activo ? true : false };
+      });
+      this.listOfDataCopy = this.listOfData;
+    });
   }
 
   openTemplate(): void {
@@ -115,119 +142,154 @@ export class DocumentComponent {
       nzTitle: 'Crear Documento',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: false }
+      nzContentParams: { edit: false },
     });
   }
 
   openEditTemplate(data: DatatableDocument): void {
-    this.edit = true
-    this.dataForm = data
+    this.edit = true;
+    this.dataForm = data;
     this.drawerRef = this.drawerService.create({
       nzTitle: 'Editar Documento',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: true }
+      nzContentParams: { edit: true },
     });
     this.drawerRef.nzOnClose.subscribe(() => {
       this.dataForm = {
         id: '',
         nombre_documento: '',
         activo: false,
-        loading: false
-      }
+        loading: false,
+      };
     });
   }
 
   changeState(dataChange: DatatableDocument): void {
-    dataChange.loading = true
+    dataChange.loading = true;
     this.serviceDocument.changeState(dataChange.id).subscribe((data: any) => {
-      dataChange.loading = false
+      dataChange.loading = false;
       if (data.ok != undefined && data.ok == false) {
-        this.message.error('<b>¡Ups!</b> Hubo un error al eliminar el documento', { nzDuration: 2500 })
-        return
+        this.message.error(
+          '<b>¡Ups!</b> Hubo un error al eliminar el documento',
+          { nzDuration: 2500 }
+        );
+        dataChange.activo = !dataChange.activo;
+        return;
       }
       if (data.resp != undefined && !data.resp) {
-        this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-        return
+        this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+          nzDuration: 2500,
+        });
+        dataChange.activo = !dataChange.activo;
+        return;
       }
-      dataChange.activo = !dataChange.activo
-      this.message.success(`<b>¡Excelente!</b> ${data.message}`, { nzDuration: 2500 })
-    })
+      this.message.success(`<b>¡Excelente!</b> ${data.message}`, {
+        nzDuration: 2500,
+      });
+    });
   }
 
   submitForm(): void {
     if (!this.validateForm.valid) {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
-          control.markAsDirty()
-          control.updateValueAndValidity({ onlySelf: true })
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
         }
       });
-      return
+      return;
     }
-    const { descripcion } = this.validateForm.value
+    const { descripcion } = this.validateForm.value;
 
     if (this.edit) {
-      this.isSaving = true
-      this.dataForm = { ...this.dataForm, nombre_documento: descripcion ? descripcion : '' }
-      this.serviceDocument.editDocument({ ...this.dataForm, nombre_documento: descripcion ? descripcion : '' })
+      this.isSaving = true;
+      this.dataForm = {
+        ...this.dataForm,
+        nombre_documento: descripcion ? descripcion : '',
+      };
+      this.serviceDocument
+        .editDocument({
+          ...this.dataForm,
+          nombre_documento: descripcion ? descripcion : '',
+        })
         .subscribe((data: any) => {
-          this.isSaving = false
+          this.isSaving = false;
           if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al editar el documento', { nzDuration: 2500 })
-            return
+            this.message.error(
+              '<b>¡Ups!</b> Hubo un error al editar el documento',
+              { nzDuration: 2500 }
+            );
+            return;
           }
           if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
+            this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+              nzDuration: 2500,
+            });
+            return;
           }
-          this.message.success('<b>¡Excelente!</b> Se actulizo el documento con exito', { nzDuration: 2500 })
-          this.drawerRef.close()
-          this.edit = false
+          this.message.success(
+            '<b>¡Excelente!</b> Se actulizo el documento con exito',
+            { nzDuration: 2500 }
+          );
+          this.drawerRef.close();
+          this.edit = false;
           this.listOfData = this.listOfData.map((data) => {
-            return (data.id == this.dataForm.id) ? { ...data, nombre_documento: this.dataForm.nombre_documento } : data
-          })
+            return data.id == this.dataForm.id
+              ? { ...data, nombre_documento: this.dataForm.nombre_documento }
+              : data;
+          });
           this.dataForm = {
             id: '',
             nombre_documento: '',
             activo: false,
-            loading: false
-          }
-        })
+            loading: false,
+          };
+        });
     } else {
-      this.isSaving = true
-      this.serviceDocument.saveDocument(descripcion)
-        .subscribe((data: any) => {
-          this.isSaving = false
-          if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al guardar el documento', { nzDuration: 2500 })
-            return
-          }
-          if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups! ${data.message}`, { nzDuration: 2500 })
-            return
-          }
-          this.message.success('<b>¡Excelente!</b> Se guardo el documento con exito', { nzDuration: 2500 })
-          this.getData()
-          this.drawerRef.close()
-        })
+      this.isSaving = true;
+      this.serviceDocument.saveDocument(descripcion).subscribe((data: any) => {
+        this.isSaving = false;
+        if (data.ok != undefined && data.ok == false) {
+          this.message.error(
+            '<b>¡Ups!</b> Hubo un error al guardar el documento',
+            { nzDuration: 2500 }
+          );
+          return;
+        }
+        if (data.resp != undefined && !data.resp) {
+          this.message.error(`<b>¡Ups! ${data.message}`, { nzDuration: 2500 });
+          return;
+        }
+        this.message.success(
+          '<b>¡Excelente!</b> Se guardo el documento con exito',
+          { nzDuration: 2500 }
+        );
+        this.getData();
+        this.drawerRef.close();
+      });
     }
   }
 
   close(): void {
-    this.drawerRef.close()
-    this.dataForm = this.globalService.createDefaultObject<DatatableDocument>(this.dataForm)
+    this.drawerRef.close();
+    this.dataForm = this.globalService.createDefaultObject<DatatableDocument>(
+      this.dataForm
+    );
   }
 
-  reset(): void{
+  reset(): void {
     this.searchValue = '';
     this.search();
   }
 
-  search(): void{
-    this.listOfData = this.listOfDataCopy.filter(({ nombre_documento }: DatatableDocument) => {
-      return nombre_documento.toLocaleUpperCase().includes(this.searchValue.toLocaleUpperCase())
-    });
+  search(): void {
+    this.listOfData = this.listOfDataCopy.filter(
+      ({ nombre_documento }: DatatableDocument) => {
+        return nombre_documento
+          .toLocaleUpperCase()
+          .includes(this.searchValue.toLocaleUpperCase());
+      }
+    );
   }
-
 }
