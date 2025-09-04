153b22878312955b6b569e2ed01da343d7b2efc0
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-09-04 12:12:11 -0500

    Se realizan ajustes de acuerdo a las recomendaciones de la herramienta WAVEE

--- src/app/pages/panel/engine/engine.component.ts
+++ src/app/pages/panel/engine/engine.component.ts
@@ -1,9 +1,22 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer';
 // interfaces
-import { DrawerFooter, drawerTemplate } from '../../../interface/global.interface';
-import { ColumnItemEngine, DatatableEngine, FormEngine } from '../../../interface/engine.interface';
+import {
+  DrawerFooter,
+  drawerTemplate,
+} from '../../../interface/global.interface';
+import {
+  ColumnItemEngine,
+  DatatableEngine,
+  FormEngine,
+} from '../../../interface/engine.interface';
 // component and-desing
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -16,9 +29,13 @@ import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzMessageModule, NzMessageService } from 'ng-zorro-antd/message';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 // services
 import { EngineService } from '../../../service/engine/engine.service';
 import { GlobalService } from '../../../service/global/global.service';
+// Directives
+import { LimitCharsDirective } from '../../../directive/limit-chars/limit-chars.directive';
+
 @Component({
   selector: 'app-engine',
   standalone: true,
@@ -36,51 +53,59 @@ import { GlobalService } from '../../../service/global/global.service';
     NzPopconfirmModule,
     NzToolTipModule,
     NzSwitchModule,
-    ReactiveFormsModule
+    NzCheckboxModule,
+    ReactiveFormsModule,
+    LimitCharsDirective,
   ],
   templateUrl: './engine.component.html',
-  styleUrl: './engine.component.css'
+  styleUrl: './engine.component.css',
 })
 export class EngineComponent {
-  @ViewChild('drawerEngine', { static: false }) drawerTemplate?: TemplateRef<drawerTemplate>
-  @ViewChild('footerEngine', { static: false }) drawerFooter?: TemplateRef<DrawerFooter>
-  isLoading: boolean = true
-  isSaving: boolean = false
-  isDeleting: boolean = false
+  @ViewChild('drawerEngine', { static: false })
+  drawerTemplate?: TemplateRef<drawerTemplate>;
+  @ViewChild('footerEngine', { static: false })
+  drawerFooter?: TemplateRef<DrawerFooter>;
+  isLoading: boolean = true;
+  isSaving: boolean = false;
+  isDeleting: boolean = false;
   searchValue: string = '';
-  drawerRef: any
+  drawerRef: any;
   dataForm: DatatableEngine = {
     id: '',
     descripcion: '',
     activo: false,
-    loading: false
-  }
-  edit: boolean = false // variable usada para saber cuando se esta actualizando el registro
+    loading: false,
+  };
+  edit: boolean = false; // variable usada para saber cuando se esta actualizando el registro
 
-  validateForm: FormGroup<FormEngine> = this.fb.group({ descripcion: ['', [Validators.required]] });
+  validateForm: FormGroup<FormEngine> = this.fb.group({
+    descripcion: ['', [Validators.required]],
+  });
 
   listOfColumns: ColumnItemEngine[] = [
     {
       name: 'id',
       sortOrder: null,
-      sortFn: (a: DatatableEngine, b: DatatableEngine) => JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
-      showSort: true
+      sortFn: (a: DatatableEngine, b: DatatableEngine) =>
+        JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
+      showSort: true,
     },
     {
       name: 'Motor',
       sortOrder: null,
-      sortFn: (a: DatatableEngine, b: DatatableEngine) => a.descripcion.localeCompare(b.descripcion),
-      showSort: true
+      sortFn: (a: DatatableEngine, b: DatatableEngine) =>
+        a.descripcion.localeCompare(b.descripcion),
+      showSort: true,
     },
     {
       name: 'Acciones',
       sortFn: null,
       sortOrder: null,
-      showSort: false
-    }
+      showSort: false,
+    },
   ];
   listOfData: DatatableEngine[] = [];
-  listOfDataCopy :DatatableEngine[] = [];
+  listOfDataCopy: DatatableEngine[] = [];
 
   constructor(
     private drawerService: NzDrawerService,
@@ -88,24 +113,27 @@ export class EngineComponent {
     private serviceEngine: EngineService,
     private message: NzMessageService,
     private globalService: GlobalService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
   }
 
   getData(): void {
-    this.isLoading = true
-    this.serviceEngine.getAllEngines()
-      .subscribe((data: any) => {
-        this.isLoading = false
-        if (data.ok != undefined && data.ok == false) {
-          this.message.error('¡Ups! Hubo un error al obtener los motores', { nzDuration: 2500 })
-          return
-        }
-        this.listOfData = data.data
-        this.listOfDataCopy = data.data
-      })
+    this.isLoading = true;
+    this.serviceEngine.getAllEngines().subscribe((data: any) => {
+      this.isLoading = false;
+      if (data.ok != undefined && data.ok == false) {
+        this.message.error('¡Ups! Hubo un error al obtener los motores', {
+          nzDuration: 2500,
+        });
+        return;
+      }
+      this.listOfData = data.data.map(({ activo, ...rest }: any) => {
+        return { ...rest, activo: activo ? true : false };
+      });
+      this.listOfDataCopy = this.listOfData;
+    });
   }
 
   openTemplate(): void {
@@ -113,108 +141,134 @@ export class EngineComponent {
       nzTitle: 'Crear Motor',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: false }
+      nzContentParams: { edit: false },
     });
   }
 
   openEditTemplate(data: DatatableEngine): void {
-    this.edit = true
-    this.dataForm = data
+    this.edit = true;
+    this.dataForm = data;
     this.drawerRef = this.drawerService.create({
       nzTitle: 'Editar Motor',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: true }
+      nzContentParams: { edit: true },
     });
     this.drawerRef.nzOnClose.subscribe(() => {
       this.dataForm = {
         id: '',
         descripcion: '',
         activo: false,
-        loading: false
-      }
+        loading: false,
+      };
     });
   }
 
   changeState(dataChange: DatatableEngine): void {
-    dataChange.loading = true
+    dataChange.loading = true;
     this.serviceEngine.changeState(dataChange.id).subscribe((data: any) => {
-      dataChange.loading = false
+      dataChange.loading = false;
       if (data.ok != undefined && data.ok == false) {
-        this.message.error('<b>¡Ups!</b> Hubo un error al eliminar el motor', { nzDuration: 2500 })
-        return
+        this.message.error('<b>¡Ups!</b> Hubo un error al eliminar el motor', {
+          nzDuration: 2500,
+        });
+        dataChange.activo = !dataChange.activo;
+        return;
       }
       if (data.resp != undefined && !data.resp) {
-        this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-        return
+        this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+          nzDuration: 2500,
+        });
+        dataChange.activo = !dataChange.activo;
+        return;
       }
-      dataChange.activo = !dataChange.activo
-      this.message.success(`<b>¡Excelente!</b> ${data.message}`, { nzDuration: 2500 })
-    })
+      this.message.success(`<b>¡Excelente!</b> ${data.message}`, {
+        nzDuration: 2500,
+      });
+    });
   }
 
   submitForm(): void {
     if (!this.validateForm.valid) {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
-          control.markAsDirty()
-          control.updateValueAndValidity({ onlySelf: true })
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
         }
       });
-      return
+      return;
     }
-    const { descripcion } = this.validateForm.value
+    const { descripcion } = this.validateForm.value;
 
     if (this.edit) {
-      this.isSaving = true
-      this.dataForm = { ...this.dataForm, descripcion: descripcion ? descripcion : '' }
-      this.serviceEngine.editEngines(this.dataForm)
-        .subscribe((data: any) => {
-          this.isSaving = false
-          if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al editar el motor', { nzDuration: 2500 })
-            return
-          }
-          if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
-          }
-          this.message.success('<b>¡Excelente!</b> Se actualizo el motor con éxito', { nzDuration: 2500 })
-          this.drawerRef.close()
-          this.edit = false
-          this.listOfData = this.listOfData.map((data) => {
-            return (data.id == this.dataForm.id) ? { ...data, descripcion: this.dataForm.descripcion } : data
-          })
-          this.dataForm = {
-            id: '',
-            descripcion: '',
-            activo: false,
-            loading: false
-          }
-        })
+      this.isSaving = true;
+      this.dataForm = {
+        ...this.dataForm,
+        descripcion: descripcion ? descripcion : '',
+      };
+      this.serviceEngine.editEngines(this.dataForm).subscribe((data: any) => {
+        this.isSaving = false;
+        if (data.ok != undefined && data.ok == false) {
+          this.message.error('<b>¡Ups!</b> Hubo un error al editar el motor', {
+            nzDuration: 2500,
+          });
+          return;
+        }
+        if (data.resp != undefined && !data.resp) {
+          this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+            nzDuration: 2500,
+          });
+          return;
+        }
+        this.message.success(
+          '<b>¡Excelente!</b> Se actualizo el motor con éxito',
+          { nzDuration: 2500 }
+        );
+        this.drawerRef.close();
+        this.edit = false;
+        this.listOfData = this.listOfData.map((data) => {
+          return data.id == this.dataForm.id
+            ? { ...data, descripcion: this.dataForm.descripcion }
+            : data;
+        });
+        this.dataForm = {
+          id: '',
+          descripcion: '',
+          activo: false,
+          loading: false,
+        };
+      });
     } else {
-      this.isSaving = true
-      this.serviceEngine.saveEngine(descripcion)
-        .subscribe((data: any) => {
-          this.isSaving = false
-          if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al guardar el motor', { nzDuration: 2500 })
-            return
-          }
-          if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
-          }
-          this.message.success('<b>¡Excelente!</b> Se guardo el motor con exito', { nzDuration: 2500 })
-          this.getData()
-          this.drawerRef.close()
-        })
+      this.isSaving = true;
+      this.serviceEngine.saveEngine(descripcion).subscribe((data: any) => {
+        this.isSaving = false;
+        if (data.ok != undefined && data.ok == false) {
+          this.message.error('<b>¡Ups!</b> Hubo un error al guardar el motor', {
+            nzDuration: 2500,
+          });
+          return;
+        }
+        if (data.resp != undefined && !data.resp) {
+          this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+            nzDuration: 2500,
+          });
+          return;
+        }
+        this.message.success(
+          '<b>¡Excelente!</b> Se guardo el motor con exito',
+          { nzDuration: 2500 }
+        );
+        this.getData();
+        this.drawerRef.close();
+      });
     }
   }
 
   close(): void {
-    this.drawerRef.close()
-    this.dataForm = this.globalService.createDefaultObject<DatatableEngine>(this.dataForm)
+    this.drawerRef.close();
+    this.dataForm = this.globalService.createDefaultObject<DatatableEngine>(
+      this.dataForm
+    );
   }
 
   reset(): void {
@@ -223,8 +277,12 @@ export class EngineComponent {
   }
 
   search(): void {
-    this.listOfData = this.listOfDataCopy.filter(({descripcion}: DatatableEngine) => {
-      return descripcion.toLocaleUpperCase().includes(this.searchValue.toLocaleUpperCase())
-    })
+    this.listOfData = this.listOfDataCopy.filter(
+      ({ descripcion }: DatatableEngine) => {
+        return descripcion
+          .toLocaleUpperCase()
+          .includes(this.searchValue.toLocaleUpperCase());
+      }
+    );
   }
 }
