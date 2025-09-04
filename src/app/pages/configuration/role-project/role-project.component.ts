8e8460fef77cee3c36918cc6890768437cc66470
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-09-04 09:09:12 -0500

    Se realizan los ajustes recomendados de la herramienta WAVE

--- src/app/pages/configuration/role-project/role-project.component.ts
+++ src/app/pages/configuration/role-project/role-project.component.ts
@@ -1,8 +1,22 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 // interfaces
-import { DrawerFooter, drawerTemplate } from '../../../interface/global.interface';
-import { ColumnItemRoleProject, DataRoleProject, DatatableRoleProject, FormRoleProject } from '../../../interface/roleproject.interface';
+import {
+  DrawerFooter,
+  drawerTemplate,
+} from '../../../interface/global.interface';
+import {
+  ColumnItemRoleProject,
+  DataRoleProject,
+  DatatableRoleProject,
+  FormRoleProject,
+} from '../../../interface/roleproject.interface';
 // component and-desing
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -17,9 +31,12 @@ import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 // services
 import { RoleProjectService } from '../../../service/role-project/role-project.service';
 import { GlobalService } from '../../../service/global/global.service';
+// Directives
+import { LimitCharsDirective } from '../../../directive/limit-chars/limit-chars.directive';
 @Component({
   selector: 'app-role-project',
   standalone: true,
@@ -38,49 +55,56 @@ import { GlobalService } from '../../../service/global/global.service';
     NzToolTipModule,
     NzSwitchModule,
     NzDropDownModule,
-    ReactiveFormsModule
+    NzCheckboxModule,
+    ReactiveFormsModule,
+    LimitCharsDirective,
   ],
   templateUrl: './role-project.component.html',
-  styleUrl: './role-project.component.css'
+  styleUrl: './role-project.component.css',
 })
 export class RoleProjectComponent {
+  @ViewChild('drawerRoleProject', { static: false })
+  drawerTemplate?: TemplateRef<drawerTemplate>;
+  @ViewChild('footerRoleProject', { static: false })
+  drawerFooter?: TemplateRef<DrawerFooter>;
+  isLoading: boolean = true;
+  isSaving: boolean = false;
 
-  @ViewChild('drawerRoleProject', { static: false }) drawerTemplate?: TemplateRef<drawerTemplate>
-  @ViewChild('footerRoleProject', { static: false }) drawerFooter?: TemplateRef<DrawerFooter>
-  isLoading: boolean = true
-  isSaving: boolean = false
-
-  drawerRef: any
+  drawerRef: any;
   dataForm: DatatableRoleProject = {
     descripcion: '',
     id: '',
     activo: false,
-    loading: false
-  }
-  edit: boolean = false // variable usada para saber cuando se esta actualizando el registro
-  searchValue: string = ''
+    loading: false,
+  };
+  edit: boolean = false; // variable usada para saber cuando se esta actualizando el registro
+  searchValue: string = '';
 
-  validateForm: FormGroup<FormRoleProject> = this.fb.group({ descripcion: ['', [Validators.required]] });
+  validateForm: FormGroup<FormRoleProject> = this.fb.group({
+    descripcion: ['', [Validators.required]],
+  });
 
   listOfColumns: ColumnItemRoleProject[] = [
     {
       name: 'id',
       sortOrder: null,
-      sortFn: (a: DatatableRoleProject, b: DatatableRoleProject) => JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
-      showSort: true
+      sortFn: (a: DatatableRoleProject, b: DatatableRoleProject) =>
+        JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
+      showSort: true,
     },
     {
       name: 'Rol',
       sortOrder: null,
-      sortFn: (a: DatatableRoleProject, b: DatatableRoleProject) => a.descripcion.localeCompare(b.descripcion),
-      showSort: true
+      sortFn: (a: DatatableRoleProject, b: DatatableRoleProject) =>
+        a.descripcion.localeCompare(b.descripcion),
+      showSort: true,
     },
     {
       name: 'Acciones',
       sortFn: null,
       sortOrder: null,
-      showSort: false
-    }
+      showSort: false,
+    },
   ];
   listOfDataCopy: DatatableRoleProject[] = [];
   listOfData: DatatableRoleProject[] = [];
@@ -91,24 +115,25 @@ export class RoleProjectComponent {
     private serviceRoleProject: RoleProjectService,
     private message: NzMessageService,
     private globalService: GlobalService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
   }
 
   getData(): void {
-    this.isLoading = true
-    this.serviceRoleProject.getAllRoleProjects()
-      .subscribe((data: any) => {
-        this.isLoading = false
-        if (data.ok != undefined && data.ok == false) {
-          this.message.error('<b>¡Ups!</b> Hubo un error al obtener los roles', { nzDuration: 2500 })
-          return
-        }
-        this.listOfData = data.data
-        this.listOfDataCopy = data.data
-      })
+    this.isLoading = true;
+    this.serviceRoleProject.getAllRoleProjects().subscribe((data: any) => {
+      this.isLoading = false;
+      if (data.ok != undefined && data.ok == false) {
+        this.message.error('<b>¡Ups!</b> Hubo un error al obtener los roles', {
+          nzDuration: 2500,
+        });
+        return;
+      }
+      this.listOfData = data.data;
+      this.listOfDataCopy = data.data;
+    });
   }
 
   openTemplate(): void {
@@ -116,95 +141,136 @@ export class RoleProjectComponent {
       nzTitle: 'Crear Rol de Proyecto',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: false }
+      nzContentParams: { edit: false },
     });
   }
 
   openEditTemplate(data: DataRoleProject): void {
-    this.edit = true
-    this.dataForm = data
+    this.edit = true;
+    this.dataForm = data;
     this.drawerRef = this.drawerService.create({
       nzTitle: 'Editar Rol de Proyecto',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: true }
+      nzContentParams: { edit: true },
     });
     this.drawerRef.nzOnClose.subscribe(() => {
-      this.dataForm = { descripcion: '', id: '', activo: false, loading: false }
+      this.dataForm = {
+        descripcion: '',
+        id: '',
+        activo: false,
+        loading: false,
+      };
     });
   }
 
   submitForm(): void {
     if (!this.validateForm.valid) {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
-          control.markAsDirty()
-          control.updateValueAndValidity({ onlySelf: true })
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
         }
       });
-      return
+      return;
     }
-    const { descripcion } = this.validateForm.value
+    const { descripcion } = this.validateForm.value;
 
     if (this.edit) {
-      this.isSaving = true
-      this.dataForm = { ...this.dataForm, descripcion: descripcion ? descripcion : '' }
-      this.serviceRoleProject.editRoleProject({ ...this.dataForm, descripcion: descripcion ? descripcion : '' })
+      this.isSaving = true;
+      this.dataForm = {
+        ...this.dataForm,
+        descripcion: descripcion ? descripcion : '',
+      };
+      this.serviceRoleProject
+        .editRoleProject({
+          ...this.dataForm,
+          descripcion: descripcion ? descripcion : '',
+        })
         .subscribe((data: any) => {
-          this.isSaving = false
+          this.isSaving = false;
           if (data.resp != undefined && data.resp == false) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
+            this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+              nzDuration: 2500,
+            });
+            return;
           }
-          this.message.success('<b>¡Excelente!</b> Se actualizo el rol con éxito', { nzDuration: 2500 })
-          this.drawerRef.close()
-          this.edit = false
+          this.message.success(
+            '<b>¡Excelente!</b> Se actualizo el rol con éxito',
+            { nzDuration: 2500 }
+          );
+          this.drawerRef.close();
+          this.edit = false;
           this.listOfData = this.listOfData.map((data) => {
-            return (data.id == this.dataForm.id) ? { ...data, descripcion: this.dataForm.descripcion } : data
-          })
-          this.dataForm = { descripcion: '', id: '', activo: false, loading: false }
-        })
+            return data.id == this.dataForm.id
+              ? { ...data, descripcion: this.dataForm.descripcion }
+              : data;
+          });
+          this.dataForm = {
+            descripcion: '',
+            id: '',
+            activo: false,
+            loading: false,
+          };
+        });
     } else {
-      this.isSaving = true
-      this.serviceRoleProject.saveRoleProject(descripcion)
+      this.isSaving = true;
+      this.serviceRoleProject
+        .saveRoleProject(descripcion)
         .subscribe((data: any) => {
-          this.isSaving = false
+          this.isSaving = false;
           if (data.resp != undefined && data.resp == false) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
+            this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+              nzDuration: 2500,
+            });
+            return;
           }
-          this.message.success('<b>¡Excelente!</b> Se guardo el rol con éxito', { nzDuration: 2500 })
-          this.getData()
-          this.drawerRef.close()
-        })
+          this.message.success(
+            '<b>¡Excelente!</b> Se guardo el rol con éxito',
+            { nzDuration: 2500 }
+          );
+          this.getData();
+          this.drawerRef.close();
+        });
     }
   }
 
   changeState(dataChange: DatatableRoleProject): void {
-    dataChange.loading = true
-    this.serviceRoleProject.changeState(dataChange.id).subscribe((data: any) => {
-      dataChange.loading = false
-      if (data.ok != undefined && data.ok == false) {
-        this.message.error('<b>¡Ups!</b> Hubo un error al eliminar la base de datos', { nzDuration: 2500 })
-        return
-      }
-      if (data.resp != undefined && !data.resp) {
-        this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-        return
-      }
-      dataChange.activo = !dataChange.activo
-      this.message.success(`<b>!Execelente!</b> ${data.message}`, { nzDuration: 2500 })
-    })
+    dataChange.loading = true;
+    this.serviceRoleProject
+      .changeState(dataChange.id)
+      .subscribe((data: any) => {
+        dataChange.loading = false;
+        if (data.ok != undefined && data.ok == false) {
+          this.message.error(
+            '<b>¡Ups!</b> Hubo un error al eliminar la base de datos',
+            { nzDuration: 2500 }
+          );
+          dataChange.activo = !dataChange.activo;
+          return;
+        }
+        if (data.resp != undefined && !data.resp) {
+          this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+            nzDuration: 2500,
+          });
+          dataChange.activo = !dataChange.activo;
+          return;
+        }
+        this.message.success(`<b>!Execelente!</b> ${data.message}`, {
+          nzDuration: 2500,
+        });
+      });
   }
 
   close(): void {
-    this.drawerRef.close()
-    this.dataForm = this.globalService.createDefaultObject<DatatableRoleProject>({
-      descripcion: '',
-      id: '',
-      activo: false,
-      loading: false
-    })
+    this.drawerRef.close();
+    this.dataForm =
+      this.globalService.createDefaultObject<DatatableRoleProject>({
+        descripcion: '',
+        id: '',
+        activo: false,
+        loading: false,
+      });
   }
 
   reset(): void {
@@ -213,8 +279,12 @@ export class RoleProjectComponent {
   }
 
   search(): void {
-    this.listOfData = this.listOfDataCopy.filter(({ descripcion }: DatatableRoleProject) => {
-      return descripcion.toLocaleUpperCase().includes(this.searchValue.toLocaleUpperCase())
-    });
+    this.listOfData = this.listOfDataCopy.filter(
+      ({ descripcion }: DatatableRoleProject) => {
+        return descripcion
+          .toLocaleUpperCase()
+          .includes(this.searchValue.toLocaleUpperCase());
+      }
+    );
   }
 }
