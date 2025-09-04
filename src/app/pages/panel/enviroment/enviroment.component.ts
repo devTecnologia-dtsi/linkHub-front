3e3008bbd8e498608ca0dedb9a2f53e5c6ab2801
andreslopezuni <andres.lopez.m@uniminuto.edu>
2025-09-04 10:26:28 -0500

    Se ajusta con las recomendaciones de la herramienta wave

--- src/app/pages/panel/enviroment/enviroment.component.ts
+++ src/app/pages/panel/enviroment/enviroment.component.ts
@@ -1,9 +1,22 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer';
 // interfaces
-import { DrawerFooter, drawerTemplate } from '../../../interface/global.interface';
-import { ColumnItemEnviroment, DatatableEnviroment, FormEnviroment } from '../../../interface/enviroment.interface';
+import {
+  DrawerFooter,
+  drawerTemplate,
+} from '../../../interface/global.interface';
+import {
+  ColumnItemEnviroment,
+  DatatableEnviroment,
+  FormEnviroment,
+} from '../../../interface/enviroment.interface';
 // component and-desing
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -16,10 +29,12 @@ import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzMessageModule, NzMessageService } from 'ng-zorro-antd/message';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 // services
 import { EnviromentService } from '../../../service/enviroment/enviroment.service';
 import { GlobalService } from '../../../service/global/global.service';
-
+// Directvices
+import { LimitCharsDirective } from '../../../directive/limit-chars/limit-chars.directive';
 
 @Component({
   selector: 'app-enviroment',
@@ -38,49 +53,57 @@ import { GlobalService } from '../../../service/global/global.service';
     NzPopconfirmModule,
     NzToolTipModule,
     NzSwitchModule,
-    ReactiveFormsModule
+    NzCheckboxModule,
+    ReactiveFormsModule,
+    LimitCharsDirective,
   ],
   templateUrl: './enviroment.component.html',
-  styleUrl: './enviroment.component.css'
+  styleUrl: './enviroment.component.css',
 })
 export class EnviromentComponent {
-  @ViewChild('drawerEnviroment', { static: false }) drawerTemplate?: TemplateRef<drawerTemplate>
-  @ViewChild('footerEnviroment', { static: false }) drawerFooter?: TemplateRef<DrawerFooter>
-  isLoading: boolean = true
-  isSaving: boolean = false
-  isDeleting: boolean = false
+  @ViewChild('drawerEnviroment', { static: false })
+  drawerTemplate?: TemplateRef<drawerTemplate>;
+  @ViewChild('footerEnviroment', { static: false })
+  drawerFooter?: TemplateRef<DrawerFooter>;
+  isLoading: boolean = true;
+  isSaving: boolean = false;
+  isDeleting: boolean = false;
   searchValue: string = '';
 
-  drawerRef: any
+  drawerRef: any;
   dataForm: DatatableEnviroment = {
     id: '',
     descripcion: '',
     activo: false,
-    loading: false
-  }
-  edit: boolean = false // variable usada para saber cuando se esta actualizando el registro
+    loading: false,
+  };
+  edit: boolean = false; // variable usada para saber cuando se esta actualizando el registro
 
-  validateForm: FormGroup<FormEnviroment> = this.fb.group({ descripcion: ['', [Validators.required]] });
+  validateForm: FormGroup<FormEnviroment> = this.fb.group({
+    descripcion: ['', [Validators.required]],
+  });
 
   listOfColumns: ColumnItemEnviroment[] = [
     {
       name: 'id',
       sortOrder: null,
-      sortFn: (a: DatatableEnviroment, b: DatatableEnviroment) => JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
-      shoWSort: true
+      sortFn: (a: DatatableEnviroment, b: DatatableEnviroment) =>
+        JSON.stringify(a.id).localeCompare(JSON.stringify(b.id)),
+      shoWSort: true,
     },
     {
       name: 'Ambiente',
       sortOrder: null,
-      sortFn: (a: DatatableEnviroment, b: DatatableEnviroment) => a.descripcion.localeCompare(b.descripcion),
-      shoWSort: true
+      sortFn: (a: DatatableEnviroment, b: DatatableEnviroment) =>
+        a.descripcion.localeCompare(b.descripcion),
+      shoWSort: true,
     },
     {
       name: 'Acciones',
       sortFn: null,
       sortOrder: null,
-      shoWSort: false
-    }
+      shoWSort: false,
+    },
   ];
   listOfData: DatatableEnviroment[] = [];
   listOfdataCopy: DatatableEnviroment[] = [];
@@ -91,25 +114,25 @@ export class EnviromentComponent {
     private serviceEnviroment: EnviromentService,
     private message: NzMessageService,
     private globalService: GlobalService
-
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
   }
 
   getData(): void {
-    this.isLoading = true
-    this.serviceEnviroment.getAllEnviroments()
-      .subscribe((data: any) => {
-        this.isLoading = false
-        if (data.ok != undefined && data.ok == false) {
-          this.message.error('¡Ups! Hubo un error al obtener los ambientes', { nzDuration: 2500 })
-          return
-        }
-        this.listOfData = data.data
-        this.listOfdataCopy = data.data
-      })
+    this.isLoading = true;
+    this.serviceEnviroment.getAllEnviroments().subscribe((data: any) => {
+      this.isLoading = false;
+      if (data.ok != undefined && data.ok == false) {
+        this.message.error('¡Ups! Hubo un error al obtener los ambientes', {
+          nzDuration: 2500,
+        });
+        return;
+      }
+      this.listOfData = data.data;
+      this.listOfdataCopy = data.data;
+    });
   }
 
   openTemplate(): void {
@@ -117,119 +140,158 @@ export class EnviromentComponent {
       nzTitle: 'Crear Ambiente',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: false }
+      nzContentParams: { edit: false },
     });
   }
 
   openEditTemplate(data: DatatableEnviroment): void {
-    this.edit = true
-    this.dataForm = data
+    this.edit = true;
+    this.dataForm = data;
     this.drawerRef = this.drawerService.create({
       nzTitle: 'Editar Ambiente',
       nzContent: this.drawerTemplate,
       nzFooter: this.drawerFooter,
-      nzContentParams: { edit: true }
+      nzContentParams: { edit: true },
     });
     this.drawerRef.nzOnClose.subscribe(() => {
       this.dataForm = {
         id: '',
         descripcion: '',
         activo: false,
-        loading: false
-
-      }
+        loading: false,
+      };
     });
   }
 
   changeState(dataChange: DatatableEnviroment): void {
-    dataChange.loading = true
+    dataChange.loading = true;
     this.serviceEnviroment.changeState(dataChange.id).subscribe((data: any) => {
-      dataChange.loading = false
+      dataChange.loading = false;
       if (data.ok != undefined && data.ok == false) {
-        this.message.error('<b>¡Ups!</b> Hubo un error al actualizar el ambiente', { nzDuration: 2500 })
-        return
+        this.message.error(
+          '<b>¡Ups!</b> Hubo un error al actualizar el ambiente',
+          { nzDuration: 2500 }
+        );
+        dataChange.activo = !dataChange.activo;
+        return;
       }
       if (data.resp != undefined && !data.resp) {
-        this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-        return
+        this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+          nzDuration: 2500,
+        });
+        dataChange.activo = !dataChange.activo;
+        return;
       }
-      dataChange.activo = !dataChange.activo
-      this.message.success(`<b>¡Excelente!</b> ${data.message}`, { nzDuration: 2500 })
-    })
+      this.message.success(`<b>¡Excelente!</b> ${data.message}`, {
+        nzDuration: 2500,
+      });
+    });
   }
 
   submitForm(): void {
     if (!this.validateForm.valid) {
-      Object.values(this.validateForm.controls).forEach(control => {
+      Object.values(this.validateForm.controls).forEach((control) => {
         if (control.invalid) {
-          control.markAsDirty()
-          control.updateValueAndValidity({ onlySelf: true })
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
         }
       });
-      return
+      return;
     }
-    const { descripcion } = this.validateForm.value
+    const { descripcion } = this.validateForm.value;
 
     if (this.edit) {
-      this.isSaving = true
-      this.dataForm = { ...this.dataForm, descripcion: descripcion ? descripcion : '' }
-      this.serviceEnviroment.editEnviroment({ ...this.dataForm, descripcion: descripcion ? descripcion : '' })
+      this.isSaving = true;
+      this.dataForm = {
+        ...this.dataForm,
+        descripcion: descripcion ? descripcion : '',
+      };
+      this.serviceEnviroment
+        .editEnviroment({
+          ...this.dataForm,
+          descripcion: descripcion ? descripcion : '',
+        })
         .subscribe((data: any) => {
-          this.isSaving = false
+          this.isSaving = false;
           if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al editar el ambiente', { nzDuration: 2500 })
-            return
+            this.message.error(
+              '<b>¡Ups!</b> Hubo un error al editar el ambiente',
+              { nzDuration: 2500 }
+            );
+            return;
           }
           if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
+            this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+              nzDuration: 2500,
+            });
+            return;
           }
-          this.message.success('<b>¡Excelente!</b> Se actualizo el ambiente con éxito', { nzDuration: 2500 })
-          this.drawerRef.close()
-          this.edit = false
+          this.message.success(
+            '<b>¡Excelente!</b> Se actualizo el ambiente con éxito',
+            { nzDuration: 2500 }
+          );
+          this.drawerRef.close();
+          this.edit = false;
           this.listOfData = this.listOfData.map((data) => {
-            return (data.id == this.dataForm.id) ? { ...data, descripcion: this.dataForm.descripcion } : data
-          })
+            return data.id == this.dataForm.id
+              ? { ...data, descripcion: this.dataForm.descripcion }
+              : data;
+          });
           this.dataForm = {
             id: '',
             descripcion: '',
             activo: false,
-            loading: false
-          }
-        })
+            loading: false,
+          };
+        });
     } else {
-      this.isSaving = true
-      this.serviceEnviroment.saveEnviroment(descripcion)
+      this.isSaving = true;
+      this.serviceEnviroment
+        .saveEnviroment(descripcion)
         .subscribe((data: any) => {
-          this.isSaving = false
+          this.isSaving = false;
           if (data.ok != undefined && data.ok == false) {
-            this.message.error('<b>¡Ups!</b> Hubo un error al guardar el ambiente', { nzDuration: 2500 })
-            return
+            this.message.error(
+              '<b>¡Ups!</b> Hubo un error al guardar el ambiente',
+              { nzDuration: 2500 }
+            );
+            return;
           }
           if (data.resp != undefined && !data.resp) {
-            this.message.error(`<b>¡Ups!</b> ${data.message}`, { nzDuration: 2500 })
-            return
+            this.message.error(`<b>¡Ups!</b> ${data.message}`, {
+              nzDuration: 2500,
+            });
+            return;
           }
-          this.message.success('<b>¡Excelente!</b> Se guardo el ambiente con éxito', { nzDuration: 2500 })
-          this.getData()
-          this.drawerRef.close()
-        })
+          this.message.success(
+            '<b>¡Excelente!</b> Se guardo el ambiente con éxito',
+            { nzDuration: 2500 }
+          );
+          this.getData();
+          this.drawerRef.close();
+        });
     }
   }
 
   close(): void {
-    this.drawerRef.close()
-    this.dataForm = this.globalService.createDefaultObject<DatatableEnviroment>(this.dataForm)
+    this.drawerRef.close();
+    this.dataForm = this.globalService.createDefaultObject<DatatableEnviroment>(
+      this.dataForm
+    );
   }
 
-  reset(): void{
+  reset(): void {
     this.searchValue = '';
     this.search();
   }
 
-  search(): void{
-    this.listOfData = this.listOfdataCopy.filter(({descripcion}: DatatableEnviroment)=>{
-      return descripcion.toLocaleLowerCase().includes(this.searchValue.toLocaleLowerCase())
-    })
+  search(): void {
+    this.listOfData = this.listOfdataCopy.filter(
+      ({ descripcion }: DatatableEnviroment) => {
+        return descripcion
+          .toLocaleLowerCase()
+          .includes(this.searchValue.toLocaleLowerCase());
+      }
+    );
   }
 }
